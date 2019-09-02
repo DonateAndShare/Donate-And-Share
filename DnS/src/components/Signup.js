3a464d728af6fa92796629e6737ace6e1831f028
Owner <you@example.com>
2019-09-03 01:43:15 +0300

    form and its childs in signup component

---
+++ DnS/src/components/Signup.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import DatePicker from "react-datepicker";
+import axios from 'axios'
+
+export default class Singup extends Component{
+    render(){
+        return(
+            <>
+              <form>
+                <input name ='firstName' placeholder='First Name' type='text' ></input>
+                <input name ='lastName' placeholder='Last Name' type='text' ></input>
+                <input name ='username' placeholder='Username' type='text' ></input>
+                <input name ='phone' placeholder='Phone Number' type='number' ></input>
+                <input name ='email' placeholder='Email' type='email' ></input>
+                <input name ='password' placeholder='Password' type='password' ></input>
+                <button type='submit'>Signup</button>
+              </form>
+            </>
+        )
+    }
+}
