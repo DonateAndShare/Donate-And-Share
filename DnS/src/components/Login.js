f5fea936fc108bdb33b75bcfb6e24cfc7f04f7c5
naaman94 <51744606+naaman94@users.noreply.github.com>
2019-09-05 12:21:23 +0300

    update login

--- DnS/src/components/Login.js
+++ DnS/src/components/Login.js
@@ -1,22 +1,73 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
+
 export default class Login extends Component{
+
+  state = {
+    username: '',
+    password: '',
+    user:[]
+  }
+
+
+   checkLogin = (e) => {
+  // this.setState({isvaled:<h1>hello</h1>})
+    // this.props.users.map(elem => {
+    //   if(e.target.name === 'username'){
+    //     console.log(e.target.value)
+    //     console.log(elem.username)
+    //     // console.log('it dosent match!!')
+    //     this.state.username = e.target.value;
+    //   }else{
+    //     let text = 'The password is incorrect! '
+    //     return text
+    //   }
+    //   if(e.target.name === 'password' ){
+    //     // alert('The password is incorrect! ')
+    //     // console.log('The password is incorrect! ')
+    //     console.log(e.target.value)
+    //     this.state.password = e.target.value 
+    //   }
+    //   // console.log(obj)
+    //   if(this.state.username === elem.username && this.state.password === elem.password){
+    //     alert('Go to the next page!')
+    //   }
+    // })
+    this.setState({ [e.target.name]: e.target.value })
+      console.log(this.state)
+  }
+
+  Login = (username, password) => {
+    console.log(username)
+    axios.get(`http://localhost:9000/login/${username}/${password}`)
+    .then(res => {
+      console.log(res.data)
+      if(res.data.length !== 0){
+        this.setState({user: res.data})
+        alert (" loged in ")
+      }else(
+        alert("no such user")
+      )
+      // this.setState({checkUser: res.data})
+    })
+     
+    console.log(this.state.user)
+  }
+
 
     render(){
         return(
             <>
-            <h3>Login Component !</h3>
             <form>
               <input name='username' type='text' placeholder='Email or Username' onChange={this.checkLogin}></input>
-              {this.state.isvaled}
               <input name='password' type='password' placeholder='Password' onChange={this.checkLogin}></input>
             </form>
             <Link to= '/users'>
-              <button onClick={this.Login}>Login</button>
+              <button onClick={this.Login.bind(this, this.state.username, this.state.password)}>Login</button>
               </Link>
-              {/* <a href='#' >Forget Password!</a> */}
+              <a href = 'mailto: DonateAndShare@gmail.com' >Forget Password!</a>
             </>
         )
     }
